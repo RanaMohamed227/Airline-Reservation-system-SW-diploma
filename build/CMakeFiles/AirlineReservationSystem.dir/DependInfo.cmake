65a7061ed3587ef51b8bac0222bef7a775a1db21
Rana Mohamed <144630250+RanaMohamed227@users.noreply.github.com>
2025-10-19 01:04:08 +0300

    Add files via upload

---
+++ build/CMakeFiles/AirlineReservationSystem.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/f/rana/SW/project3/src/admin.cpp" "CMakeFiles/AirlineReservationSystem.dir/src/admin.cpp.o" "gcc" "CMakeFiles/AirlineReservationSystem.dir/src/admin.cpp.o.d"
+  "/mnt/f/rana/SW/project3/src/aircraft.cpp" "CMakeFiles/AirlineReservationSystem.dir/src/aircraft.cpp.o" "gcc" "CMakeFiles/AirlineReservationSystem.dir/src/aircraft.cpp.o.d"
+  "/mnt/f/rana/SW/project3/src/bookingagent.cpp" "CMakeFiles/AirlineReservationSystem.dir/src/bookingagent.cpp.o" "gcc" "CMakeFiles/AirlineReservationSystem.dir/src/bookingagent.cpp.o.d"
+  "/mnt/f/rana/SW/project3/src/flight.cpp" "CMakeFiles/AirlineReservationSystem.dir/src/flight.cpp.o" "gcc" "CMakeFiles/AirlineReservationSystem.dir/src/flight.cpp.o.d"
+  "/mnt/f/rana/SW/project3/src/main.cpp" "CMakeFiles/AirlineReservationSystem.dir/src/main.cpp.o" "gcc" "CMakeFiles/AirlineReservationSystem.dir/src/main.cpp.o.d"
+  "/mnt/f/rana/SW/project3/src/maintenance.cpp" "CMakeFiles/AirlineReservationSystem.dir/src/maintenance.cpp.o" "gcc" "CMakeFiles/AirlineReservationSystem.dir/src/maintenance.cpp.o.d"
+  "/mnt/f/rana/SW/project3/src/menu.cpp" "CMakeFiles/AirlineReservationSystem.dir/src/menu.cpp.o" "gcc" "CMakeFiles/AirlineReservationSystem.dir/src/menu.cpp.o.d"
+  "/mnt/f/rana/SW/project3/src/passenger.cpp" "CMakeFiles/AirlineReservationSystem.dir/src/passenger.cpp.o" "gcc" "CMakeFiles/AirlineReservationSystem.dir/src/passenger.cpp.o.d"
+  "/mnt/f/rana/SW/project3/src/payment.cpp" "CMakeFiles/AirlineReservationSystem.dir/src/payment.cpp.o" "gcc" "CMakeFiles/AirlineReservationSystem.dir/src/payment.cpp.o.d"
+  "/mnt/f/rana/SW/project3/src/reservation.cpp" "CMakeFiles/AirlineReservationSystem.dir/src/reservation.cpp.o" "gcc" "CMakeFiles/AirlineReservationSystem.dir/src/reservation.cpp.o.d"
+  "/mnt/f/rana/SW/project3/src/systemmanager.cpp" "CMakeFiles/AirlineReservationSystem.dir/src/systemmanager.cpp.o" "gcc" "CMakeFiles/AirlineReservationSystem.dir/src/systemmanager.cpp.o.d"
+  "/mnt/f/rana/SW/project3/src/user.cpp" "CMakeFiles/AirlineReservationSystem.dir/src/user.cpp.o" "gcc" "CMakeFiles/AirlineReservationSystem.dir/src/user.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/AirlineReservationSystem.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "AirlineReservationSystem"
+  "AirlineReservationSystem.pdb"
+  "CMakeFiles/AirlineReservationSystem.dir/src/admin.cpp.o"
+  "CMakeFiles/AirlineReservationSystem.dir/src/admin.cpp.o.d"
+  "CMakeFiles/AirlineReservationSystem.dir/src/aircraft.cpp.o"
+  "CMakeFiles/AirlineReservationSystem.dir/src/aircraft.cpp.o.d"
+  "CMakeFiles/AirlineReservationSystem.dir/src/bookingagent.cpp.o"
+  "CMakeFiles/AirlineReservationSystem.dir/src/bookingagent.cpp.o.d"
+  "CMakeFiles/AirlineReservationSystem.dir/src/flight.cpp.o"
+  "CMakeFiles/AirlineReservationSystem.dir/src/flight.cpp.o.d"
+  "CMakeFiles/AirlineReservationSystem.dir/src/main.cpp.o"
+  "CMakeFiles/AirlineReservationSystem.dir/src/main.cpp.o.d"
+  "CMakeFiles/AirlineReservationSystem.dir/src/maintenance.cpp.o"
+  "CMakeFiles/AirlineReservationSystem.dir/src/maintenance.cpp.o.d"
+  "CMakeFiles/AirlineReservationSystem.dir/src/menu.cpp.o"
+  "CMakeFiles/AirlineReservationSystem.dir/src/menu.cpp.o.d"
+  "CMakeFiles/AirlineReservationSystem.dir/src/passenger.cpp.o"
+  "CMakeFiles/AirlineReservationSystem.dir/src/passenger.cpp.o.d"
+  "CMakeFiles/AirlineReservationSystem.dir/src/payment.cpp.o"
+  "CMakeFiles/AirlineReservationSystem.dir/src/payment.cpp.o.d"
+  "CMakeFiles/AirlineReservationSystem.dir/src/reservation.cpp.o"
+  "CMakeFiles/AirlineReservationSystem.dir/src/reservation.cpp.o.d"
+  "CMakeFiles/AirlineReservationSystem.dir/src/systemmanager.cpp.o"
+  "CMakeFiles/AirlineReservationSystem.dir/src/systemmanager.cpp.o.d"
+  "CMakeFiles/AirlineReservationSystem.dir/src/user.cpp.o"
+  "CMakeFiles/AirlineReservationSystem.dir/src/user.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AirlineReservationSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
